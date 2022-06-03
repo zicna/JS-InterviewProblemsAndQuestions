56ee16b39fb51edbe1dbfc41b9d9d6ae006feb94
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-06-03 09:24:48 -0500

    first commit

---
+++ sumOneOrMore.js
@@ -0,0 +1,4 @@
+// Napisite sum metodu koja ce raditi pravilno kada je pozovete koristeci neku od sintaksi ispod
+// console.log(sum(2,3));   // Outputs 5
+// console.log(sum(2)(3));  // Outputs 5
+
