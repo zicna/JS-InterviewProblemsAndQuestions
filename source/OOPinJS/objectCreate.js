b52006586ac59cae272605cd943c742ce58bcdb9
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-06-17 16:05:06 -0500

    inheritance using Object.create()

--- source/OOPinJS/objectCreate.js
+++ source/OOPinJS/objectCreate.js
@@ -1,3 +1,4 @@
+// * parent class prototype
 const PersonProto = {
     calcAge(){
         return new Date().getFullYear() - this.birthYear;
@@ -10,7 +11,32 @@ const PersonProto = {
     }
 }
 
-const john = Object.create(PersonProto);
-john.init("John", "Do", 1990);
+// * child class prototype
+// * creating an empty object with __proto__ property of PersonProto 
+const StudentProto = Object.create(PersonProto)
+// * creating john object with __proto__ property of StudentProto
+// * With this prototype chain is established 
 
-console.log(john.calcAge())
+StudentProto.init = function(firstName, lastName, birthYear, course){
+    PersonProto.init.call(this, firstName, lastName, birthYear)
+    this.course = course
+}
+
+StudentProto.introduce = function(){
+    console.log(`My name is ${this.firstName} and I study ${this.course}`)
+}
+
+const john = Object.create(StudentProto)
+
+john.init("John", "Do", 1990, "CS")
+john.introduce()
+
+
+
+
+// StudentProto.prototype = Object.create(PersonProto)
+
+// const john = Object.create(StudentProto);
+// john.init("John", "Do", 1990);
+
+// console.log(john.calcAge())
