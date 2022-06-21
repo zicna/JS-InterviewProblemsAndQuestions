59adb5ed6fe799177b610de8cfd2c69f128226da
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-06-21 11:48:02 -0500

    add modernJS folder

--- source/browsersAPI/geolocation.js
+++ source/browsersAPI/geolocation.js
@@ -1,4 +1,4 @@
-const map = document.getElementById("map");
+const map = document.getElementById('map')
 
 if (navigator.geolocation) console.dir(navigator.geolocation)
 navigator.geolocation.getCurrentPosition(
@@ -8,17 +8,33 @@ navigator.geolocation.getCurrentPosition(
     // L global variable form leaflet we added in our script that we can use in all our files
     // ! any variable that is global in any script will be avaiable to all our scripts as long as we try to access it after that script was loaded
     const coors = [latitude, longitude]
-    const map = L.map('map').setView(coors, 13)
+    const map = L.map('map').setView(coors, 10)
+    // console.dir(map)
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map)
 
-    L.marker(coors)
-      .addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup()
+    map.on('click', function (mapEvent) {
+      
+      // * mapEvent is event created by leaflet
+      // console.log(mapEvent)
+      // const { lat, lng } = mapEvent.latlng
+    //   L.marker([lat, lng])
+    //     .addTo(map)
+    //     .bindPopup(
+    //       L.popup({
+    //         maxWidtht: 250,
+    //         minWidth: 100,
+    //         autoClose: false,
+    //         closeOnClick: false,
+    //         className: 'popup',
+    //       })
+    //     )
+    //     .setPopupContent('Workout')
+    //     .openPopup()
+    // })
   },
   function () {
     alert('could not get your position')
---
+++ source/modernJS/restPatern.js
@@ -0,0 +1,6 @@
+// * rest patern
+const rest = (...inputs) => inputs;
+
+const returnFromRest = rest(1,2,3,4,5,6);
+
+console.log(returnFromRest)
