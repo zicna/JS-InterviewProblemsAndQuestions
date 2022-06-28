e1a93d88035b967db0a80daa413f137d01368c2d
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-06-28 14:41:21 -0500

    add 'proof' that export/ import is a live connection

--- source/modules/script.js
+++ source/modules/script.js
@@ -1,4 +1,4 @@
-import User, {printName} from './user.js'
+import User, {printName, allUsers, addUser, printAllUsers} from './user.js'
 
 console.log('hello from import')
 
@@ -7,3 +7,12 @@ const milan = new User('Milan', 35)
 console.log(milan)
 
 printName(milan)
+
+// * export / import is live connection 
+console.log(allUsers) //=> []
+
+printAllUsers();
+addUser(milan)
+printAllUsers();
+
+console.log(allUsers) //=> [User]
--- source/modules/user.js
+++ source/modules/user.js
@@ -1,5 +1,7 @@
 console.log("hello from export")
 
+export const allUsers = [];
+
  class User{
     constructor(name, age){
         this.name = name
@@ -11,6 +13,20 @@ function printName(user){
     console.log(`My name is ${user.name}`)
 }
 
-export {printName};
+function addUser(user){
+    allUsers.push(user)
+}
+
+function printAllUsers(){
+    if(allUsers.length == 0){
+        console.log("no users found")
+        return;
+    }
+    for(const user of allUsers){
+        console.log(`${user.name}`)
+    }
+}
+
+export {printName, addUser, printAllUsers};
 export default User;
 
