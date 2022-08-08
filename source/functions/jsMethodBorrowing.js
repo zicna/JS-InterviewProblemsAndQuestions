b5ddf1288caed0f0edc1c95eb0331438a2eca3c5
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-08-08 10:16:50 -0500

    add example for method borrowing in JS

---
+++ source/functions/jsMethodBorrowing.js
@@ -0,0 +1,27 @@
+"use strict"
+const jonas ={
+  firstName: "Jonas",
+  lastName: "Jo",
+  year: 1989,
+  calcAge: function(){
+    console.log(this);
+    // console.log(new Date().getFullYear() - this.year)
+  }
+}
+
+jonas.calcAge();
+
+const matilda ={
+  firstName: "Matilda",
+  lastName: "Do",
+  year: 1999
+} 
+
+// method borrowing
+
+matilda.calcAge = jonas.calcAge;
+
+matilda.calcAge();
+
+const f = matilda.calcAge;
+f();
